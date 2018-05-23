3f368510a03062f705ad3014bacfecd2c7c822de
Jae Bradley <jae.b.bradley@gmail.com>
2018-05-23 01:49:07 -0400

    refactor(search): remove search command
    BREAKING CHANGE: remove search command such that urban dictionary cli is triggered by udict only

--- src/executables/udict.js
+++ src/executables/udict.js
@@ -1,10 +1,20 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import { promptSearchTerm } from '../services/prompters/searchTermPrompter';
 
 import pkg from '../../package.json';
 
+const executeSearch = async () => {
+  try {
+    await promptSearchTerm();
+  } catch (e) {
+    console.error(`Rut ro - there was an error: ${e}`); // eslint-disable-line no-console
+  }
+};
+
 program.version(pkg.version)
   .description('UrbanDictionary search from the command line')
-  .command('search', 'Search on Urban Dictionary')
   .parse(process.argv);
+
+executeSearch();
